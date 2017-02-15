3df0baec84ac1a832dae1c3c739380099f523cb3
sriram298 <sriramh46@gmail.com>
2017-02-15 20:03:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,12 +81,22 @@ return htmltemplate ;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter= counter+1;
+    res.send(counter.tostring())
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName= req.params.articleName;
     res.send(createtemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
